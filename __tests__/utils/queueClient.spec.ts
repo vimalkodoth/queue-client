6e6d8b951c3b68c608d4c34de1787b3e9f4ab01e
Vimal Madhavan <Vimal.Madhavan@intigral.net>
2022-10-12 01:16:45 +0400

    Added unit tests

--- __tests__/utils/queueClient.spec.ts
+++ __tests__/utils/queueClient.spec.ts
@@ -1,9 +1,44 @@
-import { PriorityQueue } from 'queue-client';
-import { QueueClient } from 'queue-client';
+import { PriorityQueue, QueueClient } from 'queue-client';
 
-test('Sample', () => {
-  const priorityQueue = new PriorityQueue();
-  const qClient = new QueueClient(priorityQueue);
+describe('As a developer, I want to check findTopN ', () => {
+  let priorityQueue: PriorityQueue | null;
+  let queueClient: QueueClient | null;
 
-  expect(qClient.findTopN([1, 2, 5, 3, 1], 2)).toEqual([5, 3]);
+  beforeAll(() => {
+    priorityQueue = new PriorityQueue();
+    queueClient = new QueueClient(priorityQueue);
+  });
+
+  afterAll(() => {
+    priorityQueue = null;
+    queueClient = null;
+  });
+
+  it('to work as expected', () => {
+    const expected = [5, 4];
+    const topN = queueClient?.findTopN([1, 2, 5, 3, 4], 2);
+    expect(topN).toEqual(expected);
+  });
+
+  it('to include repeition of priority(number) in the result', () => {
+    const expected = [6, 5, 5];
+    const topN = queueClient?.findTopN([1, 5, 5, 6, 4], 3);
+    expect(topN).toEqual(expected);
+  });
+
+  it('to returns all numbers in array as expected when N is larger than array size', () => {
+    const expected = [5, 4, 1];
+    const topN = queueClient?.findTopN([1, 5, 4], 5);
+    expect(topN).toEqual(expected);
+  });
+
+  it('to throw an error when input is empty array', () => {
+    const expected = new Error('input is empty!');
+    expect(() => queueClient?.findTopN([], 5)).toThrow(expected);
+  });
+
+  it('to filter input to contain only finite numbers', () => {
+    const expected = [5, 3, 1, 1];
+    expect(queueClient?.findTopN([1, 5, 3, 'a', 1], 5)).toEqual(expected);
+  });
 });
