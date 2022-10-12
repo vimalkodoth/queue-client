81469383cb51676ab78f5276f3abae0f6854ccdd
Vimal Madhavan <Vimal.Madhavan@intigral.net>
2022-10-12 19:36:44 +0400

    Removed output lib

--- lib/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './utils/priorityQueue';
-export { default as QueueClient } from './utils/queueClient';
--- lib/utils/queueClient.d.ts
+++
@@ -1,7 +0,0 @@
-import { PriorityQueue } from '@vimalkodoth/queue-client';
-export default class QueueClient {
-    pQueue: PriorityQueue | null;
-    constructor(pQueue: PriorityQueue);
-    findTopN(inputNumbers: Array<any>, N: number): Array<number>;
-    static getFilteredNumbers(inputList?: Array<any>): Array<number>;
-}
